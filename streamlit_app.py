5e176b29f5ebd6f87981f6c3cb2a409f731580af
N3wee <ox24oy1i@students.codeinstitute.net>
2025-06-14 13:31:18 +0000

    Set app title and icon, reorder Streamlit pages for better UX

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+st.set_page_config(
+    page_title="Heritage Housing Estimator",
+    page_icon="🏠",
+    layout="centered"
+)
 from app_pages.multipage import MultiPage  
 from app_pages.page_data_visuals import page_data_visuals_body
 from app_pages.page_project_summary import page_project_summary_body
@@ -6,13 +11,10 @@ from app_pages.page_price_prediction import page_price_prediction_body
 from app_pages.page_home import page_home_body
 from app_pages.page_hypothesis import page_hypothesis_body
 
-
-
 app = MultiPage(app_name="Heritage Housing Price Estimator")
 
-app.add_page("🏠 Home", page_home_body)
 
-app.add_page("🏠 Project Summary", page_project_summary_body)
+app.add_page("🏠 Home", page_home_body)
 
 app.add_page("📊 Data Insights", page_data_visuals_body)
 
@@ -20,5 +22,6 @@ app.add_page("📊 Hypotheses & Validation", page_hypothesis_body)
 
 app.add_page("🏷️ Predict House Price", page_price_prediction_body)
 
+app.add_page("🏠 Project Summary", page_project_summary_body)
 
 app.run()
